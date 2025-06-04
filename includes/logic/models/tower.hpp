135008a8f745d7b0d2ebb87e7aaf8a4c7149b9ee
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-06-04 13:49:05 +0200

    feat: created basic tower class

---
+++ includes/logic/models/tower.hpp
@@ -0,0 +1,24 @@
+#ifndef TOWER_H
+#define TOWER_H
+
+#include <iostream>
+#include <string>
+
+class Tower{
+    public:
+    std::string name;
+    int hp=100;
+    int atk=1;
+    int level=1;
+    double atkMultiplier=0.1;
+    bool visible=false;
+
+    Tower(std::string name, int hp, int atk);
+
+    void attack(Enemy* enemy);
+    void upgrade();
+};
+
+
+
+#endif
---
+++ source/logic/models/tower.cpp
@@ -0,0 +1,20 @@
+#include "tower.hpp"
+
+Tower::Tower(std::string name, int hp, int atk) {
+    this->name = name;
+    this->hp = hp;
+    this->atk = atk;
+
+    if (hp<=0) {
+        std::cout<<"Tower "<<this->name<<" has been destroyed"<<std::endl;
+    }
+}
+
+void Tower::attack(Enemy* enemy) {
+    enemy->hp-=this->atk;
+}
+
+void Tower::upgrade() {
+    this->level+=1;
+    this->atk=this->atk*this->atkMultiplier;
+}
