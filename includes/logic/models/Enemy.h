478ca22d8c8b7dc2e5e7c581367020c0151000e6
christine chemali <christine.chemali@laplateforme.io>
2025-06-04 12:42:44 +0200

    Add structure folder and files and gitignore to new branch

---
+++ includes/logic/models/Enemy.h
@@ -0,0 +1,8 @@
+//
+// Created by chris on 04/06/2025.
+//
+
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#endif //ENEMY_H
---
+++ includes/logic/models/Wave.h
@@ -0,0 +1,8 @@
+//
+// Created by chris on 04/06/2025.
+//
+
+#ifndef WAVE_H
+#define WAVE_H
+
+#endif //WAVE_H
