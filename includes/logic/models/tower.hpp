84e08a4f136a6b46192165502775071e375b86f7
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-06-05 10:20:38 +0200

    feat: added DamageBase, getBaseHealth and a spritePath attribute

--- includes/logic/models/tower.hpp
+++ includes/logic/models/tower.hpp
@@ -3,17 +3,33 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
+#include "position.h"
 
 class Tower{
-    public:
+private:
+    static Tower instance; //for singleton
+    int baseHealth;
+
+    Tower(): baseHealth(100){};
+
+public:
+    static Tower& getInstance() {
+        static Tower instance;
+        return instance;
+    }
+    void damageBase(int damage);
+    int getBaseHealth() const;
+
     std::string name;
     int hp=100;
     int atk=1;
     int level=1;
     double atkMultiplier=0.1;
     bool visible=true;
+    std::string spritePath;
 
-    Tower(std::string name, int hp, int atk);
+    Tower(std::string name, int hp, int atk, std::string spritePath);
 
     void attack(Enemy* enemy);
     void upgrade();
