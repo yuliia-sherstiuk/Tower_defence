e2338eb8a8a82811a2c9ffafd1f3154115d2ea6a
christine chemali <christine.chemali@laplateforme.io>
2025-06-12 08:49:29 +0200

    Add loadSound method to manage soundbuffer and attache it to the sound - init sound objects with the corresponding soundbuffeer usinf setbuffer - ensured that sound volume is set based on the master volume when loading sounds

--- source/logic/controllers/SoundController.cpp
+++ source/logic/controllers/SoundController.cpp
@@ -16,3 +16,15 @@ SoundController::SoundController()
         currentMusic = std::make_unique<sf::Music>();
     };
 
+bool SoundController::loadSound(const std::string& name, const std::string& filepath) {
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile(filepath)) {
+        return false;
+    }
+
+    soundBuffers[name] = std::move(buffer);
+    sounds[name] = std::make_unique<sf::Sound>();
+    sounds[name]->setBuffer(soundBuffers[name]);
+    sounds[name]->setVolume(soundVolume*(masterVolume/100.0f));
+    return true;
+}
