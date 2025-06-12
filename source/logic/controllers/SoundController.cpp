823375117b40735aa3ae269db9b8567e763cf427
christine chemali <christine.chemali@laplateforme.io>
2025-06-12 08:51:50 +0200

    add comments

--- source/logic/controllers/SoundController.cpp
+++ source/logic/controllers/SoundController.cpp
@@ -6,16 +6,19 @@
 #include <iostream>
 #include <algorithm>
 
+// Returns the singleton instance of the SoundController.
 SoundController &SoundController::getInstance() {
     static SoundController instance;
     return instance;
 }
 
+// Initializes the SoundController with default volume levels and sets up the music player.
 SoundController::SoundController()
     : masterVolume(100.0f), soundVolume(100.0f), musicVolume(100.0f), muted(false), savedMasterVolume(100.0f) {
         currentMusic = std::make_unique<sf::Music>();
     };
 
+// Loads a sound from a file and associates it with a specified name.
 bool SoundController::loadSound(const std::string& name, const std::string& filepath) {
     sf::SoundBuffer buffer;
     if (!buffer.loadFromFile(filepath)) {
