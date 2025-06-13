c8f24dfac3f5709b6b4c4e69cce8085c21c205f2
christine chemali <christine.chemali@laplateforme.io>
2025-06-13 13:22:05 +0200

    Implement: sets up and register ui buttons for the window view - assign and bound a unique id

--- source/logic/controllers/EventController.cpp
+++ source/logic/controllers/EventController.cpp
@@ -6,8 +6,20 @@
 #include <iostream>
 #include <algorithm>
 
+// Constructor for EventController class. Initializes mouse position coordinates to (0,0) and
+// sets dragging volume state to false. Sets up all window view buttons upon creation.
 EventController::EventController()
     : mousePosition(0,0), isDraggingVolume(false) {
     setupWindowViewButtons();
+}
+
+//Sets up and register Ui buttons for the window view : each button is assigned a unique ID and bounds
+void EventController::setupWindowViewButtons() {
+    //difficulty buttons - exact WindowView //update if needed: difficultyButtons
+    for (int i = 0; i <3; i++) {
+        std::string buttonId = "difficulty_" + std::to_string(i+1);
+        sf::FloatRect bounds(890 + i*35, 350 ,30,30);
+        registerUIButton(buttonId, bounds);
+    }
 }
 
