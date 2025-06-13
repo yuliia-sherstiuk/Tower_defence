6d2aa422f18a4a98b79b1198f93008e4083c0a96
christine chemali <christine.chemali@laplateforme.io>
2025-06-13 10:40:37 +0200

    Fix : change way to handle position to make it compatible with existing ui file

--- includes/logic/controllers/EventController.h
+++ includes/logic/controllers/EventController.h
@@ -10,16 +10,25 @@
 #include <map>
 #include <string>
 #include <vector>
-#include "../utils/Position.h"
+//#include "../utils/Position.h"
+
+//Check if this is the best solution or use existing Position file ?
+//Simple position for compatibility with existing Ui
+struct Position {
+    float x, y;
+    Position(float x, float y) : x(x), y(y) {}
+};
 
 class EventController {
 public:
 
     //Callbacks
     using KeyCallback = std::function<void(sf::Keyboard::Key)>;
-    using MouseCallback = std::function<void(Position, sf::Mouse::Button)>;
-    using MouseMoveCallback = std::function<void(Position)>;
+    using MouseClickCallback = std::function<void(sf::Vector2f,sf::Mouse::Button)>;//sf::Vector2f instead of Position
+    using MouseMoveCallback = std::function<void(sf::Vector2f)>;
     using ButtonCallback = std::function<void(const std::string&)>;
+    using VolumeCallback = std::function<void(float)>; // for slider volume
+    using ScrollCallback = std::function<void(bool)>; // true = up, false = down
 
     EventController();
 };
